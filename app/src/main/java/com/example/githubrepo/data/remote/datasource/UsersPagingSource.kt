5f7653bbebf4799c67f32bde9522e9d78f3fb1c0
Olorunishola Habeeb <olorunisholahabeeb@mail.com>
2024-07-08 14:52:57 +0100

    search users Paging source

---
+++ app/src/main/java/com/example/githubrepo/data/remote/datasource/UsersPagingSource.kt
@@ -0,0 +1,47 @@
+package com.example.githubrepo.data.remote.datasource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.githubrepo.data.remote.UserApi
+import com.example.githubrepo.domain.model.User
+import kotlinx.coroutines.delay
+import retrofit2.HttpException
+import java.lang.Exception
+
+class SearchUsersPagingSource(
+    private val api: UserApi,
+    private val searchQuery: String,
+) : PagingSource<Int, User>() {
+
+    override fun getRefreshKey(state: PagingState<Int, User>): Int? {
+        return state.anchorPosition?.let { anchorPage ->
+            val page = state.closestPageToPosition(anchorPage)
+            page?.nextKey?.minus(1) ?: page?.prevKey?.plus(1)
+        }
+    }
+
+    private var totalUsersCount = 0
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
+        val page = params.key ?: 1
+        return try {
+            val userResponse = api.searchUsers(searchQuery = searchQuery, page = page)
+            totalUsersCount += userResponse.items.size
+            val users = userResponse.items.distinctBy { it.login } //Remove duplicates
+
+            LoadResult.Page(
+                data = users,
+                nextKey = if (totalUsersCount == userResponse.total_count) null else page + 1,
+                prevKey = null
+            )
+        } catch (e: HttpException){
+            e.printStackTrace()
+            LoadResult.Error(throwable = e)
+        }catch (e: Exception) {
+            e.printStackTrace()
+            LoadResult.Error(throwable = e)
+        }
+    }
+
+
+}
